1f26e570663cc7c5c175d61478fa84832ace02e3
gaurav0903 <saxena090320expert@gmail.com>
2022-03-04 00:41:17 +0530

    change

---
+++ 3_Implemantation/src/test.c
@@ -0,0 +1,31 @@
+#include "game.h"
+#include "unity.h"
+
+void test_searchrecords(void);
+
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+int main()
+{
+/* Initiate the Unity Test Framework */
+  UNITY_BEGIN();
+
+/* Run Test functions */
+  RUN_TEST(test_searchrecords);
+
+  /* Close the Unity Test Framework */
+  return UNITY_END();
+}
+
+void test_searchrecords(void)
+{
+    char name[20]="Gaurav";
+    int i=50;
+    TEST_ASSERT_EQUAL(50, i);
+    TEST_ASSERT_EQUAL("Gaurav", name);
+
+   
+}
